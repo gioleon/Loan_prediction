82eb4d31a110c3327e38d0ba9539891902acba85
gioleon <giovannileon2017@gmail.com>
2022-02-18 07:37:34 -0500

    Correcion de errores en la creacion de API

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# @app.post('/pred')
+# def index(nombre: str):
+#     return nombre
+
 @app.post('/make_pred')
 def make_pred(gender: str, married: str, education: str, credit: int, property_area: str):
 
@@ -20,32 +24,32 @@ def make_pred(gender: str, married: str, education: str, credit: int, property_a
 
     # OneHotEncoding
     encoded_features = pd.DataFrame(
-        encoder.transform(df[['Gender', 'Married', 'Education', 'Property_Area']]).toarray(), columns=encoder.get_feature_names(['Gender',
-                                                                                                                                 'Married',
-                                                                                                                                 'Education',
-                                                                                                                                 'Property_Area']))
+        encoder.transform(df[['Gender', 'Married', 'Education', 'Property_Area']]).toarray(), columns=encoder.get_feature_names_out(['Gender',
+                                                                                                                                     'Married',
+                                                                                                                                     'Education',
+                                                                                                                                     'Property_Area']))
 
     # Build final df
 
     df.drop(['Gender', 'Education', 'Married',
             'Property_Area'], axis=1, inplace=True)
 
-    df2 = pd.concant([encoded_features, df], axis=1)
+    df2 = pd.concat([encoded_features, df], axis=1)
 
     pred = model.predict(df2)
 
-    return pred[0]
+    return print(f'El resultado es: {pred[0]}')
 
 
-gender = 'Male'
-married = 'Yes'
-education = 'Graduate'
-credit = 1
-property_area = 'Urban'
+# gender = 'Male'
+# married = 'Yes'
+# education = 'Graduate'
+# credit = 1
+# property_area = 'Urban'
 
-url = f'http://127.0.0.1:8000/make_pred?gender={gender}&married={married}&education={education}&credit={credit}&property_area={property_area}'
-url = url.replace(' ', '20')
+# url = f'http://127.0.0.1:8000/make_pred?gender={gender}&married={married}&education={education}&credit={credit}&property_area={property_area}'
+# url = url.replace(' ', '20')
 
-resp = requests.post(url)
+# resp = requests.post(url)
 
-print(resp.content)
+# print(resp.content)
