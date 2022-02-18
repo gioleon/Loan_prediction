09e7fcc3eaf3e625403a61871d90efbd3c16588d
gioleon <giovannileon2017@gmail.com>
2022-02-17 22:45:45 -0500

    Creacion API v.3

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+import pickle
+import pandas as pd
+import streamlit as st
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post('/make_pred')
+def make_pred(gender: str, married: str, education: str, credit: int, property_area: str):
+
+    # Load files
+    model = pd.read_pickle('pickles/model.pkl')
+    encoder = pd.read_pickle('pickles/encoder.pkl')
+
+    # df
+    df = pd.DataFrame([[gender, married, education, credit, property_area]],
+                      columns=['Gender', 'Married', 'Education', 'Credit_History', 'Property_Area'])
+
+    # OneHotEncoding
+    encoded_features = pd.DataFrame(
+        encoder.transform(df[['Gender', 'Married', 'Education', 'Property_Area']]).toarray(), columns=encoder.get_feature_names(['Gender',
+                                                                                                                                 'Married',
+                                                                                                                                 'Education',
+                                                                                                                                 'Property_Area']))
+
+    # Build final df
+
+    df.drop(['Gender', 'Education', 'Married',
+            'Property_Area'], axis=1, inplace=True)
+
+    df2 = pd.concant([encoded_features, df], axis=1)
+
+    pred = model.predict(df2)
+
+    return pred[0]
+
+
+gender = 'Male'
+married = 'Yes'
+education = 'Graduate'
+credit = 1
+property_area = 'Urban'
+
+url = f'http://127.0.0.1:8000/make_pred?gender={gender}&married={married}&education={education}&credit={credit}&property_area={property_area}'
+url = url.replace(' ', '20')
+
+resp = requests.post(url)
+
+print(resp.content)
